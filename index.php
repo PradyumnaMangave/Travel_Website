7dbdf5f3978fdce7cb61090dc048b5ccb26eae62
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-08-22 15:03:59 +0530

    connected to a database and wrote php code

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php
+$insert = false;
+if(isset($_POST['name'])){
+
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "trip_db";
+
+    $con = mysqli_connect($server, $username, $password, $dbname);
+
+    if(!$con){
+        die("Connection to this database is failed due to " . mysqli_connect_error());
+    }
+
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $desc= $_POST['desc'];
+
+    $sql = "INSERT INTO `us trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+    
+
+    if($con->query($sql) == true){
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> " . $con->error;
+    }
+
+    $con->close();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +43,7 @@
     <title>Travel form Website</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&family=Sriracha&display=swap" rel="stylesheet">   
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&family=Sriracha&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -14,7 +51,12 @@
     <div class = "container">
         <h3>Welcome to USA trip form </h3>
         <p>Enter your details and submit this form to confirm your participation in the trip.</p>
-        <p class="submitMsg">Thank You For Submitting This Form. See You On Our Trip.</p>
+
+        <?php
+        if($insert == true){
+        echo "<p class='submitMsg'>Thank You For Submitting This Form. See You On Our Trip.</p>"; 
+    }
+        ?>
         <form action="index.php" method="post">
             <input type="text" name="name" id="name" placeholder="Enter Your Name">
             <input type="text" name="age" id="age" placeholder="Enter Your Age">
@@ -23,9 +65,7 @@
             <input type="phone" name="phone" id="phone" placeholder="Enter Your Phone Number">
             <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter Any other Information here"></textarea>
             <button class="btn">Submit</button>
-            
         </form>
-    
     </div>
     <script src = "index.js"></script>
 </body>
